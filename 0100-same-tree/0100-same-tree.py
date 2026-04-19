4e9988e225fb85779a508d420dcec7e910deb034
Spandan139 <Spandanbasu139work@gmail.com>
2026-04-20 03:45:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -7,6 +7,4 @@ class Solution:
             return False
 
         if p.val == q.val:
-            self.isSameTree(p.left,q.left)
-            self.isSameTree(p.right,q.right)
-        return p.val == q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+            return p.val == q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
